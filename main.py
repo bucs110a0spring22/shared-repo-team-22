2aa43a4016696fc7a80c6d1ac94ec5b1574d6624
Jiarong Jin <98533787+jjin16@users.noreply.github.com>
2022-02-08 15:44:50 +0000

    1. fixed input statement error (added string) 2. continued on the project and added for loop to print out the list items on seperate lines. 3. Swapped first and last element on the list. 4. print statement on the mylist to confirm the swap

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+mylist = []
+for i in range(4):
+  user_number= int(input("Enter an number: "))
+  mylist.append(user_number)
+#print(mylist)
+position=0
+for i in range (4):
+  print(mylist[position])
+  position+=1
+mylist[0], mylist[3]= mylist[3], mylist[0]
+print(mylist)
